11181dbf8b85afe8cd6a6926f12af6e27af8107d
Saeedeh Karimi <saeedehkarimi1994@gmail.com>
2025-04-09 20:21:31 +0200

    used from keyframe

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,44 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
+    screens: {
+      sm: "340px",
+      md: "640px",
+      lg: " 768px",
+      xl: "1180px"
+    },
     extend: {},
+    container: {
+      center: true,
+      padding: {
+        default: "12px",
+        md: "32px",
+      }
+    },
+    fontFamily: {
+      sans: ["Poppins", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+      mono: ["Menlo", "monospace"],
+    },
+    keyframes: {
+      move: {
+        "50%": { transform: "translateY(-1rem)" },
+      },
+      rotate: {
+        "0%": { transform: "rotate(0deg)" },
+        "100%": { transform: "rotate(360deg)" },
+      },
+      scaleUp: {
+        "0%": { transform: "scale(0.8)" },
+        "50%": { transform: "scale(1.2)" },
+        "100%": { transform: "scale(0.8)" },
+      }
+    },
+    animation: {
+      movingY: "move 3s linear infinite",
+      rotating: "rotate 15s linear infinite",
+      scalingUp: "scaleUp 3s linear infinite",
+    }
   },
   plugins: [],
 }
